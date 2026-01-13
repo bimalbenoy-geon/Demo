25c2d2805212b2c495add1ff092de9c59c32d381
Bimal Benoy <bimal.benoy@gadgeon.inc>
2026-01-13 10:54:07 +0530

    just to test stash

--- hello_world/src/main/java/com/example/App.java
+++ hello_world/src/main/java/com/example/App.java
@@ -7,7 +7,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("version 1");
-        System.out.println("version 2");
         
         
 
